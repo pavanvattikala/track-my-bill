f9c1eb9fee94abb9c6def8534b30041d4ff51c7a
Pavan Vattikla <pavanvattikala54@gmail.com>
2025-11-02 17:10:55 +0530

    Added PWA

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,18 +9,33 @@ const geistSans = Geist({
   subsets: ["latin"],
 });
 
+const defaultUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : "http://localhost:3000";
+
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL(defaultUrl),
   title: "Track My Bill",
   description:
     "Next.js application for tracking expenses and managing bills with Artificial Intelligence.",
   icons: {
-    icon: "/favicon.ico",
-    shortcut: "/track-my-bill.png",
+    icon: [
+      {
+        url: "/web-app-manifest-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        url: "/web-app-manifest-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
   },
 };
 
--- app/manifest.ts
+++ app/manifest.ts
@@ -3,23 +3,22 @@ import { MetadataRoute } from "next";
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: "Track My Bill",
-    short_name: "BillTracker",
+    short_name: "TrackMyBill",
     description: "AI Powered application for tracking expenses.",
     start_url: "/",
     display: "standalone",
     background_color: "#ffffff",
-    theme_color: "#0070f3",
+    theme_color: "#000000",
     icons: [
       {
-        src: "/track-my-bill.png",
-        sizes: "512x512",
+        src: "/web-app-manifest-192x192.png",
+        sizes: "192x192",
         type: "image/png",
       },
       {
         src: "/track-my-bill.png",
         sizes: "512x512",
         type: "image/png",
-        purpose: "maskable",
       },
     ],
   };
